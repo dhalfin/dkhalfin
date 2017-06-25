66dc9dadac29fb4fa18ffcec3b443e019aeb9872
Dinar Khalfin <dinarhalfin@yahoo.com>
2017-06-25 17:21:01 +0300

    Added Hello, World!

---
+++ src/ru/job4j/Calculate.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+/**
+ * Calculate.
+ *
+ * @author Dinar Khalfin (dinarhalfin@yahoo.com)
+ * @version $Id$
+ * @since 25.06.2017
+ */
+public class Calculate{
+	/**
+     * Main.
+     * @param args - args.
+     */
+	public static void main(String[] args){
+		System.out.println("Hello, World!");
+	}
+}
