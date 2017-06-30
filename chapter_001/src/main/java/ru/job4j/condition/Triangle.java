75d183bbf0c53ea4a18d7754bf151e22d0929e34
Dinar Khalfin <dinarhalfin@yahoo.com>
2017-06-30 13:20:34 +0300

    update Triangle and TriangleTest

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -32,21 +32,21 @@ public class Triangle {
 	}
 	/**
 	 * Check for area of triangle.
-     * @param a - a.
-	 * @param b - b.
-	 * @param c - c.
 	 * @return area.
 	 */
-	public double area(double a, double b, double c) {
-        double a = Math.sqrt((a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()));
-        double b = Math.sqrt((a.getX() - c.getX()) * (a.getX() - c.getX()) + (a.getY() - c.getY()) * (a.getY() - c.getY()));
-        double c = Math.sqrt((b.getX() - c.getX()) * (b.getX() - c.getX()) + (b.getY() - c.getY()) * (b.getY() - c.getY()));
+
+	public double area() {
+        double ab = Math.sqrt((a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()));
+        double ac = Math.sqrt((a.getX() - c.getX()) * (a.getX() - c.getX()) + (a.getY() - c.getY()) * (a.getY() - c.getY()));
+        double bc = Math.sqrt((b.getX() - c.getX()) * (b.getX() - c.getX()) + (b.getY() - c.getY()) * (b.getY() - c.getY()));
 		boolean go = true;
-		if (a + b <= c || a + c <= b || a + c <= b) {
+		if (ab + ac <= bc || ab + ac <= bc || ab + ac <= bc) {
             go = false;
         } else {
-		double p = (a + b + c) / 2.0;
-        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		double p = (ab + bc + ac) / 2.0;
+        return Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
 	}
+
+
  }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.number.IsCloseTo.closeTo;
 import static org.junit.Assert.assertThat;
 /**
 * Test.
@@ -18,8 +18,8 @@ public class TriangleTest {
 	@Test
 	public void whenAreaSetThreePointsThenTriangleArea() {
 		Point a = new Point(0, 0);
-		Point a = new Point(0, 2);
-		Point a = new Point(2, 0);
+		Point b = new Point(0, 2);
+		Point c = new Point(2, 0);
 		Triangle triangle = new Triangle(a, b, c);
 		double result = triangle.area();
 		double expected = 2D;
