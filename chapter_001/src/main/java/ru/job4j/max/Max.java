2deb2b684194448dea74822271c4aa7a80d0c0bd
Dinar Khalfin <dinarhalfin@yahoo.com>
2017-07-04 11:43:01 +0300

    update for Max

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -8,25 +8,23 @@ package ru.job4j.max;
  */
 public class Max {
 	/**
-     * Result.
-     */
-    private int result;
-	/**
 	 * Maximum.
      * @param first - first.
 	 * @param second - second.
 	 * @return result.
 	 */
     public int max(int first, int second) {
-
         result = (first > second) ? first : second;
         return result;
     }
 	/**
-	 * Get the result.
-	 * @return result.
+	 * Maximum.
+     * @param first - first.
+	 * @param second - second.
+	 * @param third - third.
+	 * @return maximum number.
 	 */
-	public int getResult() {
-        return this.result;
-    }
+	public int max(int first, int second, int third) {
+		return max(max(first, second), third);
+	}
 }
